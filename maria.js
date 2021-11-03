d524ac5e3751a624907c7bdd41d2d41b8edd0ee2
mariasolsikke <mariasolsikke@gmail.com>
2021-11-03 13:19:04 +0100

    IGANG MED JAVASCRIPT

---
+++ maria.js
@@ -0,0 +1,7 @@
+function document.getElementById
+
+
+var navn = document.getElementById ('fname')
+var email= document.getElementById ('email-imput')
+var besked = document.getElementById ('message-tous')
+
