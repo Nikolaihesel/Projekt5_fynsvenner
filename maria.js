72f8041ca015b44646fa07abb494f4e99bf65e81
mariasolsikke <mariasolsikke@gmail.com>
2021-11-03 14:20:20 +0100

    kæmper ihærdigt med javascript. Pop op dukker hurtigt op, forsvinder hurtigt igen og siden refreshes.

--- maria.js
+++ maria.js
@@ -1,7 +1,29 @@
-function document.getElementById
+
+var navn = document.getElementById ('fname');
+var email= document.getElementById ('email-imput');
+var besked = document.getElementById ('message-tous');
+var knap = document.getElementById ('submit-btn-contact');
+var popper = document.getElementById('popupcon');
+var beskedinfo = [navn, email];
+console.log('link');
+function tak(){
+    
+    popper.style.display = "block";
+/*
+    if (navn.value.length == 0 && email.value.length == 0 && besked.value.length == false) {
+        
+        consol.log ('emt')
+    }
+
+    else {
+       
+        consol.log ('works')
+
+    }
+
+    */
 
 
-var navn = document.getElementById ('fname')
-var email= document.getElementById ('email-imput')
-var besked = document.getElementById ('message-tous')
+}
+
 
