5c72f936c16af88becbaaba2e893652f5c3a6e5c
mariasolsikke <mariasolsikke@gmail.com>
2021-11-04 12:46:10 +0100

    Javascript færdigt. Kontakt formular funger og pop op besked kommer

--- maria.js
+++ maria.js
@@ -1,31 +1,44 @@
+function msgConfirm(){
 
-var navn = document.getElementById ('fname');
-var email= document.getElementById ('email-imput');
-var besked = document.getElementById ('message-tous');
-var knap = document.getElementById ('submit-btn-contact');
-var popper = document.getElementById('popupcon');
+let navn = document.getElementById ('fname').value;
+let email= document.getElementById ('email-input').value;
+let popper = document.getElementById('popupcon');
+let besked = 'Vi har modtaget din besked og vi vil vende tilbage til dig hurtigst muligt. Vi har sendt et kvittering på at beskeden er sendt til din email';
 
-var beskedinfo = [navn, email];
-console.log('link');
-function tak(){
+
+
+var beskedinfo = [navn, besked, email];
+
+console.log(beskedinfo);
     
     popper.style.display = "block";
-/*
-    if (navn.value.length == 0 && email.value.length == 0 && besked.value.length == false) {
-        
-        consol.log ('emt')
-    }
-
-    else {
-       
-        consol.log ('works')
-
-    }
-
-    */
 
 
-}
+
+    const menu = document.getElementById('text-area-popup');
+beskedinfo.forEach((field)=> {
+
+    menu.appendChild(createMenuItem(field));
+})
+};
+
+
+function createMenuItem(field) {
+    let p = document.createElement('p');
+    p.classList.add("listItem_confirm");
+    p.textContent = field;
+    return p;
+
+};
+
+
+
+
+
+
+
+
+
 
 var lukKnap = document.getElementById('close-btn')
 
@@ -33,7 +46,29 @@ var lukKnap = document.getElementById('close-btn')
 
         lukKnap.style.display = "none"
 
-    }
+    };
+
+
+
+     // if (navn.value.length == 0 && email.value.length == 0 && besked.value.length == false) {
+        
+    //     consol.log ('emt')
+    // }
+
+    // else ( navn.value.lenght < 2 && email.value.lenght < 2 && besked.value.length < 2 == true){
+       
+    //     consol.log ('works')
+
+    // }
+
+
+    let form = document.getElementById("maria-form");
+
+function handleForm(event) {
+     event.preventDefault(); } 
+
+form.addEventListener('submit', handleForm);
+
 
 
 
